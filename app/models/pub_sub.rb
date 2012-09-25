53d845a1009a54dfb7471d5dc3e34b26e61ee02e
Jamil Bou Kheir <jamilboukheir@me.com>
2012-09-25 16:48:40 -0700

    best effort to unsubscribe

--- app/models/pub_sub.rb
+++ app/models/pub_sub.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 class PubSub < ActiveRecord::Base
   attr_accessible :blog_url, :status
   before_update :check_subscription
-  before_destroy :unsubscribe
+  before_destroy :unsubscribe!
   
   # if URI can't understand it it's not a valid URL
   validates_format_of :blog_url, with: URI::regexp(%w(http https))
@@ -16,6 +16,11 @@ class PubSub < ActiveRecord::Base
           has an RSS/Atom feed and the feed contains <link rel='hub'...")
       end
     end
+  end
+  
+  def unsubscribe!
+    unsubscribe
+    true
   end
   
   def subscribe(url = self.blog_url)

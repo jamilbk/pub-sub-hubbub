79810f957cd6fd9614a2450582d6ad1963153bb6
Jamil Bou Kheir <jamilboukheir@me.com>
2012-09-25 17:05:38 -0700

    PostgreSQL doesn't like text field types

--- db/migrate/20120925235329_add_document_to_pub_subs.rb
+++ db/migrate/20120925235329_add_document_to_pub_subs.rb
@@ -1,5 +1,10 @@
 class AddDocumentToPubSubs < ActiveRecord::Migration
   def change
-    add_column :pub_subs, :document, :text, :limit => 4294967295 # longtext
+    case connection.adapter_name.downcase.to_sym
+    when :mysql # dammit mysql
+      add_column :pub_subs, :document, :text, :limit => 4294967295 # longtext
+    else
+      add_column :pub_subs, :document, :text
+    end
   end
 end
